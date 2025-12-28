2093b3134b80dabe5d2b353944cb319b567b0c5f
chams jlassi <chamsjlassi2@gmail.com>
2025-12-28 03:12:27 +0100

    full backend with jwt and init frontend

---
+++ book-network/src/main/java/com/pfe/book/book/Book.java
@@ -0,0 +1,53 @@
+package com.pfe.book.book;
+
+import com.pfe.book.common.BaseEntity;
+import com.pfe.book.feedback.Feedback;
+import com.pfe.book.history.BookTransactionHistory;
+import com.pfe.book.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+
+    @Transient
+    public double getRate() {
+        if (feedbacks == null || feedbacks.isEmpty()) {
+            return 0.0;
+        }
+        return this.feedbacks.stream()
+                .mapToInt(Feedback::getNote)
+                .average()
+                .orElse(0.0);
+    }
+}
