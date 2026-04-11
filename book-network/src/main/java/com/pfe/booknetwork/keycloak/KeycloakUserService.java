2fb49c173d22b3398e92eb76a8b225026287cfb7
chams jlassi <chamsjlassi2@gmail.com>
2026-04-11 02:08:43 +0100

    fixing address

--- book-network/src/main/java/com/pfe/booknetwork/keycloak/KeycloakUserService.java
+++ book-network/src/main/java/com/pfe/booknetwork/keycloak/KeycloakUserService.java
@@ -1,6 +1,8 @@
 package com.pfe.booknetwork.keycloak;
 
+import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,17 +16,19 @@ public class KeycloakUserService {
     private String realm;
 
     public KeycloakUserService(
-            @Value("${keycloak.auth-server-url}") String authServerUrl,
-            @Value("${keycloak.admin-client-id}") String clientId,
-            @Value("${keycloak.admin-client-secret}") String clientSecret
+            @Value("${keycloak.auth-server-url}") String authServerUrl
+
     ) {
-        this.keycloak = Keycloak.getInstance(
-                authServerUrl,
-                "master",        // always master realm for admin API
-                clientId,
-                clientSecret,
-                "master"
-        );
+
+            this.keycloak = KeycloakBuilder.builder()
+                    .serverUrl(authServerUrl)
+                    .realm("master")              // ← always master for admin
+                    .clientId("admin-cli")        // ← default client, always exists
+                    .username("admin")            // ← your KEYCLOAK_ADMIN
+                    .password("admin")            // ← your KEYCLOAK_ADMIN_PASSWORD
+                    .grantType(OAuth2Constants.PASSWORD)
+                    .build();
+
     }
 
     public String getUserFullName(String userId) {
